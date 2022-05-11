fff977b048759f53f66140f918f5e70e62485407
Peter Wen <peter.wen@razer.com>
2022-05-11 11:23:48 +0800

    init QYExternalAccessory project

---
+++ Example/Pods/Target Support Files/Pods-QYExternalAccessory_Example/Pods-QYExternalAccessory_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QYExternalAccessory_Example : NSObject
+@end
+@implementation PodsDummy_Pods_QYExternalAccessory_Example
+@end
---
+++ Example/Pods/Target Support Files/QYExternalAccessory/QYExternalAccessory-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QYExternalAccessory : NSObject
+@end
+@implementation PodsDummy_QYExternalAccessory
+@end
